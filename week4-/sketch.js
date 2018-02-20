d72935f234ef769fb7de86c76838203fdbf936c9
Qinglin Wang <wangq814@newschool.edu>
2018-02-19 23:43:26 -0500

    working on it..

--- week4-/sketch.js
+++ week4-/sketch.js
@@ -5,7 +5,7 @@ function setup(){
 createCanvas(800, 800);
 let numVehicles = 15;
 for (let i = 0; i<numVehicles;i++){
-// v = new Vehicle(createVector(width/2, height/2));
+
 v = new Vehicle(createVector(random(0,width),random(0,height)))
 v.color = color(random(0,255), random(0,255),random(0,255));
 v.maxSpeed =  random(1,8);
@@ -18,11 +18,10 @@ v = new Vehicle(createVector(width/2, height/2));
 function draw(){
   background(255);
 
-  fill(200,200,200,100);
-  stroke(0);
-  strokeWeight(2);
-  ellipse(mouseX, mouseY,48,48);
-  //update and display vehicle
+  // fill(200,200,200,100);
+  // stroke(0);
+  // strokeWeight(2);
+  // ellipse(mouseX, mouseY,48,48);
   for (let v of vehicles) {
   v.seek(createVector(mouseX, mouseY)); 
   v.update();
@@ -30,14 +29,14 @@ function draw(){
 }
 }
 
-//define vehicle class
+
 class Vehicle{
 constructor(position){
-//this is where we define our properties
+
 this.position = position;
 this.velocity = createVector(0,0);
 this.acceleration = createVector(0,0);
-// r is our size
+
 this.r = 6;
 this.color = color(255,0,0);
 
@@ -58,7 +57,7 @@ applyForce(force){
 
 }
 
-//update
+
 update() {
 
 this.velocity.add(this.acceleration);
@@ -66,14 +65,11 @@ this.position.add(this.velocity);
 this.acceleration.mult(0);
 }
 
-//display
-display(){
-	//draw a triangle rotated in the direction of celocity 
 
-	//get the angle from velocity 
+display(){
+	
 	let theta = this.velocity.heading()+HALF_PI;
 
-	//set drawing properties
 	fill(this.color);
 	stroke(0);
 	strokeWeight(1);
@@ -81,7 +77,7 @@ display(){
 	rotate(theta);
 
 
-    //draw the vehicle 
+
 	beginShape();
     vertex(0, -this.r*2);
     vertex(-this.r,this.r*2);
